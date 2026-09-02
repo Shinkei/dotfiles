c5dd5e37b7b1f337a6fa6e36d9cd8203b6d6082e
Jorge Ramirez <348883+Shinkei@users.noreply.github.com>
2026-09-02 22:41:22 +0200

    Add AI skills and color theme gruvbox for nvim

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    lazy = false,
+    config = true,
+    opts = {
+      contrast = "hard",
+      transparent_mode = false,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
